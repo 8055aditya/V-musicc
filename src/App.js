6e35ee1f2c37839cde47eb8b7761f23c85fa188b
8055aditya <adityarangari03@gmail.com>
2024-05-31 13:07:32 +0530

    music commit

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [keyword,setKeyword] = useState("")
+  const [tracks, setTracks] = useState([]);
+  const [isLoading,setIsLoading] = useState(false)
+
+  const getTracks = async ()=>{
+
+    setIsLoading(true)
+    let data =await fetch(`https://v1.nocodeapi.com/aditya11999988/spotify/SoBWqTEAAvpSNOWn/search?q=${keyword ===""?"trending":keyword}&type=track`);
+
+    let convertedData =await data.json();
+    // console.log(convertedData.tracks.items);
+    setTracks(convertedData.tracks.items);
+    setIsLoading(false)
+  }
+
+  // useEffect(()=>{
+  //   getTracks()
+  // },[])
+
+  return (
+    <>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+        V-music
+        </a>
+      {/* <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button> */}
+        <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
+          <input
+            value={keyword}
+            onChange={(event)=>setKeyword(event.target.value)}
+            className="form-control me-2 w-75"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button onClick={getTracks} className="btn btn-outline-success">
+            Search
+          </button>
+        </div>
+      </div>
+      </nav>
+
+      <div className="container">
+        <div className={`${isLoading ?"" :"d-none"}`}>
+          <div className="col-12 py-5 text-center">
+            <div
+            className="spinner-border"
+            style={{ width: "3rem", height: "3rem" }}
+            role="status"
+            >
+            <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={`${keyword==="" ?"" :"d-none"}`}>
+          <div className="col-12 py-5 text-center">
+            <h1>v-music</h1>
+          </div>
+        </div>
+      
+        <div className="row">
+          {tracks.map((element) => {
+
+            return (
+              <div key={element.id} className="col-lg-3 col-md-6 py-2">
+                {/* <img
+                  className='border-2 w-100'
+                  src={element.album.images[0].url}
+                  alt=""
+                />
+                <p>{element.id}</p> */}
+
+                <div className="card">
+                  <img src={element.album.images[0].url} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">{element.name}</h5>
+                      <p className="card-text">
+                        Artist: {element.album.artists[0].name}
+                      </p>
+                      <p className="card-text">
+                        Release date: {element.album.release_date}
+                      </p>
+                      <audio src={element.preview_url} controls className="w-100"></audio>
+                    </div>
+                  </div>
+
+              </div>
+                    )})}
+        </div>
+      </div>
+    </>
+    
+  );
+}
+
+export default App;
